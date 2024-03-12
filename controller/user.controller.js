c42c1bcb6b1464b32aab912e4d7157ad061177cb
Anne-Sophie <annesophie.faustino@gmail.com>
2024-03-12 22:34:37 +0400

    user route and model

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,14 +1,77 @@
 import UserModel from "../model/user.model.js";
 import bcrypt from "bcrypt";
 
+async function getAllUsers(req, res) {
+  try {
+    const getAllUsers = await UserModel.findAll({
+      where: { activated: 1, role: "Prestataire" },
+    });
+
+    if (getAllUsers.length === 0) {
+      return res.status(200).json({ message: "Aucun utilisateur inscrit." });
+    }
+    return res.status(200).json(getAllUsers);
+  } catch (error) {
+    return res.status(500).json({
+      message: error,
+    });
+  }
+}
 async function getUserNotActivated(req, res) {
   try {
-    const getUsers = await UserModel.findAll({where: { activated: 0 }})
+    const getUserNotActivated = await UserModel.findAll({
+      where: { activated: 0 },
+    });
 
-    if(getUsers.length === 0) {
-      return res.status(200).json({ message: "Aucune demande d'inscription à validé."})
+    if (getUserNotActivated.length === 0) {
+      return res
+        .status(200)
+        .json({ message: "Aucune demande d'inscription à validé." });
     }
-    return res.status(200).json(getUsers);
+    return res.status(200).json(getUserNotActivated);
+  } catch (error) {
+    return res.status(500).json({
+      message: error,
+    });
+  }
+}
+
+async function deleteUser(req, res) {
+  try {
+    const deleteUser = await UserModel.destroy({
+      where: { id: req.params.id },
+    });
+
+    if (!deleteUser) {
+      res.status(400).json({ message: "Cet utilisateur n'existe pas" });
+    }
+
+    return res.status(200).json({ message: "Le match à bien été supprimé" });
+  } catch (error) {
+    return res.status(500).json({
+      message: error,
+    });
+  }
+}
+
+async function validateAccount(req, res) {
+  try {
+    const validateAccount = await UserModel.update(
+      {
+        activated: 1,
+      },
+      {
+        where: { id: req.params.id },
+      }
+    );
+
+    if (!validateAccount) {
+      res.status(400).json({ message: "Ce compte n'existe pas" });
+    }
+
+    return res.status(200).json({
+      message: "L'utilisateur a bien été mis à jour..",
+    });
   } catch (error) {
     return res.status(500).json({
       message: error,
@@ -22,7 +85,8 @@ async function createUser(req, res) {
     const addUser = await UserModel.create({
       email: req.body.email,
       password: await bcrypt.hash(req.body.password, saltRounds),
-      role: req.body.role
+      role: req.body.role,
+      activated: req.body.activated,
     });
 
     console.log(addUser);
@@ -35,4 +99,10 @@ async function createUser(req, res) {
   }
 }
 
-export { createUser, getUserNotActivated };
+export {
+  createUser,
+  getUserNotActivated,
+  validateAccount,
+  getAllUsers,
+  deleteUser,
+};
--- routes/user.route.js
+++ routes/user.route.js
@@ -2,11 +2,34 @@ import express from "express";
 import {
   createUser,
   getUserNotActivated,
+  validateAccount,
+  getAllUsers,
+  deleteUser,
 } from "../controller/user.controller.js";
+import { checkRolesToken, userLog, verifyToken } from "../middleware/auth.js";
 
 const UserRoutes = express.Router();
 
+UserRoutes.get("/users", getAllUsers);
+UserRoutes.get("/user", verifyToken, userLog);
 UserRoutes.post("/register", createUser);
-UserRoutes.get("/users-not-activated", getUserNotActivated);
+UserRoutes.get(
+  "/users-not-activated",
+  getUserNotActivated,
+  verifyToken,
+  checkRolesToken(["Admin"])
+);
+UserRoutes.put(
+  "/validate-account/:id",
+  validateAccount,
+  verifyToken,
+  checkRolesToken(["Admin"])
+);
+UserRoutes.delete(
+  "/users/delete/:id",
+  deleteUser,
+  verifyToken,
+  checkRolesToken(["Admin", "Prestataire"])
+);
 
 export default UserRoutes;
